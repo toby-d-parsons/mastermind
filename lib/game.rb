d52c416c877fa044052dabe5601789b73b45c5ad
Toby Parsons <toby.d.parsons@gmail.com>
2024-07-16 13:27:37 +0100

    feat(Game): add turn method with input validation and make choices an instance variable
    - Added turn method to take user input and validate it
    - Validation checks if input is 4 characters long and starts with a valid color character
    - If input is invalid, re-requests user input
    - Made choices an instance variable for better accessibility within the class

--- lib/game.rb
+++ lib/game.rb
@@ -2,12 +2,24 @@ class Game
     def initialize
         # @board = Array.new(4, '-')
             # perhaps each turn creates a new array and pushes it into this? Yes, can have a turns left counter as well
-        # @turns = 6
+        @turns_remaining = 6
             # In future, allow initialize to set the number of turns for the game?
         @code = [get_random_color, get_random_color, get_random_color, get_random_color]
     end
+    def turn
+        input = gets.chomp.split('')
+        is_valid?(input) ? true : turn && return
+        @turns_remaining -= 1
+    end
+    def is_valid?(input_arr)
+        input_arr.length == 4 &&
+        input_arr.all? do |val|
+            valid_color_abbrv = @choices.map { |x| x.to_s[0] }
+            valid_color_abbrv.any? { |arr| arr == val}
+        end
+    end
     def get_random_color
-        choices = ['red', 'green', 'blue', 'yellow', 'orange', 'pink', 'grey', 'white']
-        choices.sample
+        @choices = ['red', 'green', 'blue', 'yellow', 'orange', 'pink', 'grey', 'white']
+        @choices.sample
     end
 end
--- main.rb
+++ main.rb
@@ -2,3 +2,9 @@ require 'pry-byebug'
 require_relative 'lib/game.rb'
 
 game = Game.new
+
+game.turn
+
+# binding.pry
+
+p game
